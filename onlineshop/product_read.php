0b8fb4d2e032c61abcdf6f454aae104c06da572c
January <jiayin202003@dasein.edu.my>
2022-11-22 08:52:43 +0800

    week7_update

--- onlineshop/product_read.php
+++ onlineshop/product_read.php
@@ -99,8 +99,6 @@
         </div>
     </div> <!-- end .container -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
-    </script>
 
 </body>
 
